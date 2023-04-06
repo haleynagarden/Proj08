7a8324361b78a8d719f03cc61e46df4b6122c2e1
haleynagarden <119707970+haleynagarden@users.noreply.github.com>
2023-04-06 14:18:25 -0500

    Add files via upload

---
+++ Proj08Runner.java
@@ -0,0 +1,23 @@
+
+public class Proj08Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		public String run(String str) {
+			return "I certify that this program is my own work\r\n"
+					+ "and is not the work of others. I agree not\r\n"
+					+ "to share my solution with others.\r\n"
+					+ "Replace this line with your name";
+	}
+		
+		String run(int i, String s) {
+			return "Haleyna Garden";
+		}
+		
+		byte run(int i) {
+			return (byte)i;
+		}
+
+	}
+}
